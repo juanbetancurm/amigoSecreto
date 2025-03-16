f938f38af0c6c255bc6753c3b602c5c31550289a
juanjbetancur <juan.jbetancur852@gmail.com>
2025-03-16 12:51:22 -0500

    Lista working

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    
+    const inputAmigo = document.getElementById('amigo');
+    const nombreAmigo = inputAmigo.value.trim();
+    
+    
+    if (nombreAmigo === '') {
+        alert('Por favor, ingrese un nombre válido');
+        return;
+    }
+    
+    amigos.push(nombreAmigo);
+    
+    actualizarListaAmigos();
+    
+    inputAmigo.value = '';
+    
+    inputAmigo.focus();
+}
+
+function actualizarListaAmigos() {
+    const listaAmigos = document.getElementById('listaAmigos');
+    
+    listaAmigos.innerHTML = '';
+    
+    amigos.forEach(amigo => {
+        const li = document.createElement('li');
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
